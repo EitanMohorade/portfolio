a82582bb56b8b832fc35ecc8025ed579c810efd2
Eitanmohorade <eitanluc@gmail.com>
2024-01-11 13:08:21 -0300

    adapting portfolio

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,3 @@
-const date = document.getElementById('date')
-const setCurrentYear = () => {
-	const currentYear = new Date().getFullYear()
-	date.textContent = currentYear
-}
-setCurrentYear()
-
 // hamburger menu
 const hamburger = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav-list')
